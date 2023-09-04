d644c9ea8773b911d4cb003622ac545d96d3807b
ana-ju98 <106592930+ana-ju98@users.noreply.github.com>
2023-09-04 09:43:20 -0300

    Update processainscricao.php

--- processainscricao.php
+++ processainscricao.php
@@ -1,17 +1,25 @@
 <?php
     include_once("conexao.php");
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $datainicio = date("y:m:d h:i:s");
-    $dataultima = date("y:m:d h:i:s");
-    $senha=$_POST['senha'];
-    $situacoe_id=$_POST['situacoe_id'];
-    $niveis_acesso_id=$_POST['niveis_acesso_id'];
+    $1SIM= $_POST['1SIM'];
+    $2NAO = $_POST['2NAO'];
+    $3SIM = $_POST['3SIM'];
+    $4NAO= $_POST['4NAO'];
+    $5SIM = $_POST['5SIM'];
+    $6NAO = $_POST['6NAO'];
+    $7SIM = $_POST['7SIM'];
+    $8NAO = $_POST['8NAO'];
+    $9SIM = $_POST['9SIM'];
+    $10NAO = $_POST['10NAO'];
+    $11SIM = $_POST['11SIM'];
+    $12NAO = $_POST['12NAO'];
+    $TALVEZ= $_POST['TALVEZ'];
+    $SOLTIRO = $_POST['SOLTEIRO']; 
+    $CASADO = $_POST['CASADO'];
+    $VIÚVA= $_POST['VIÚVA'];
 
 
-$result_cadastro = "INSERT INTO usuarios (nome, email, created, modified, senha, 
-situacoe_id, niveis_acesso_id) VALUES ('$nome', '$email', '$datainicio','$dataultima', 
-'$senha', '$situacoe_id', '$niveis_acesso_id')";    
+$result_cadastro = "INSERT INTO usuarios (1SIM, 2NAO,3SIM,4NAO,5SIM,6NAO, 7SIM, 8NAO, 9SIM, 10NAO, 11SIM, 12NAO, TALVEZ, SOLTEIRO, CASADO, VIÚVA) 
+VALUES ('$1SIM', '$2NAO','$3SIM','$4NAO','$5SIM','$6NAO', '$7SIM', '$8NAO', '$9SIM', '$10NAO', '$11SIM', '$12NAO', '$TALVEZ', '$SOLTEIRO', '$CASADO', '$VIÚVA' )";    
 
 $resultado_cadastro = mysqli_query($conn, $result_cadastro);
 $ultimo_id = mysqli_insert_id($conn);
